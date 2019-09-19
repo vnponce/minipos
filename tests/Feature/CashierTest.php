54d0a0f732cd8e433a9eb0b5a0c9f7df4298f5a0
Abel Ponce <vnponce8@gmail.com>
2019-09-18 23:41:12 -0500

    Initialize and write first test

---
+++ tests/Feature/CashierTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CashierTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $user = new User;
+        $this->actingAs($user, 'api');
+    }
+
+    /** @test */
+    public function get_cashier_balance()
+    {
+//        $cashier = create(Cashier::class);
+        $date = Carbon::create(2019, 06, 11, 12, 45);
+        factory(Cashier::class)->create([
+            'date_open' => $date,
+            'value_previous_close' => 6248,
+            'value_open' => null,
+            'observation' => ''
+        ]);
+        $response = $this->get('/api/v1/balance');
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status' => 'Success',
+            'results' => [
+                'date_open' => '2019/06/11',
+                'hour_open' => '12:45',
+                'value_previous_close' => 6248,
+                'value_open' => null,
+                'observation' => ''
+            ]
+        ]);
+    }
+}
---
+++ tests/utilities/functions.php
@@ -0,0 +1,5 @@
+<?php
+function create($class, $attributes = [])
+{
+    return factory($class)->create($attributes);
+}
